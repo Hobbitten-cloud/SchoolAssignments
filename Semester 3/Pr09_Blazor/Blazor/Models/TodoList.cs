4028ab872eef892c989e6681079bce421e3c40d9
Hobbitten <janenhansen@gmail.com>
2025-11-05 13:09:51 +0100

    Learning about blazor - over halfway through the assignments today

---
+++ Semester 3/Pr09_Blazor/Blazor/Models/TodoList.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.Models
+{
+    public class TodoList
+    {
+        public int TodoId { get; set; }
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Semester 3/Pr09_Blazor/Blazor/Models/TodoListRepo.cs
@@ -0,0 +1,22 @@
+﻿namespace Blazor.Models
+{
+    public static class TodoListRepo
+    {
+        public static List<TodoList> todoLists = new List<TodoList>
+        {
+            new TodoList { TodoId = 1, Task = "Task 1", IsCompleted = false, DateTime = DateTime.Now },
+            new TodoList { TodoId = 2, Task = "Task 2", IsCompleted = true, DateTime = DateTime.Now },
+            new TodoList { TodoId = 3, Task = "Task 3", IsCompleted = false, DateTime = DateTime.Now }
+        };
+
+        public static List<TodoList> GetAllTodos()
+        {
+            return todoLists.OrderBy(t => t.IsCompleted).ToList();
+        }
+
+        public static void AddTodo(TodoList todo)
+        {
+            todoLists.Add(todo);
+        }
+    }
+}
