c60d0a6da9ca1b894275f51806a98b7dae56b462
Hobbitten <janenhansen@gmail.com>
2025-11-06 13:12:25 +0100

    Technology work today

---
+++ Semester 3/Teknologi-faget/Encryption/Method.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class Method
+    {
+        public bool IsPrime(uint number)
+        {
+            if (number == 0 || number == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You cannot write 0 or 1");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+
+            for (uint i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(number + " is not a prime number");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return false;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(number + " is a prime number");
+            Console.ForegroundColor = ConsoleColor.White;
+            return true;
+        }
+
+        public List<uint> GetPrimeFactors(uint number)
+        {
+            //Signatur: public List<uint> GetPrimeFactors(uint number)
+            //Precondition: Parameteren number er større end nul
+            //Postcondition: Der returneres en liste bestående af alle primtalsfaktorer for number
+            //Eksempel: 	GetPrimeFactors(3713) returnerer[47, 79]
+
+            //GetPrimeFactors(2380) returnerer[2, 5, 7, 17]
+            if (number == 0 || number == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You cannot write 0 or 1");
+                Console.ForegroundColor = ConsoleColor.White;
+                return null;
+            }
+
+
+        }
+    }
+}
---
+++ Semester 3/Teknologi-faget/Encryption/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Encryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+
+            Method method = new Method();
+
+            while (continueProgram == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Wwrite 0 to exit");
+                Console.Write("Write a number: ");
+                uint userInput = Convert.ToUInt32(Console.ReadLine());
+
+                if (userInput == 0)
+                {
+                    continueProgram = false;
+                    Environment.Exit(0);
+                }
+
+                method.IsPrime(userInput);
+                Console.WriteLine();
+                Console.Write("Press anything to continue");
+                Console.ReadLine();
+            }
+        }
+    }
+}
