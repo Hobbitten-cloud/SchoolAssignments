d0fc93bdb8f7d3e4fec6442dc4d1ebf25875d03b
Hobbitten <janenhansen@gmail.com>
2025-12-01 09:19:04 +0100

    Client stuff

---
+++ Semester 3/Teknologi-faget/ClientCommunication/Client/Client.cs
@@ -0,0 +1,119 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+
+namespace Client {
+	internal class Client {
+		internal int N, E;
+
+		static void Main(string[] args) {
+			Client p = new Client();
+			p.Run();
+		}
+
+		private void Run() {
+			//Pre : None
+			//Post: Connects to server using socket, id is read from keyboard and sent to server,
+			//      a message holding servers public key is read and stored in N and E. Messages 
+			//      are read from keyboard, ":" (delimiter char) are replaced, message is converted
+			//      to upper case, encrypted and sent. When message is "STOP" connection is closed
+
+			Socket serverConnection = GetConnection();
+			String id = ReadText(" Enter ID : ");
+			serverConnection.Send(Encoding.ASCII.GetBytes(id));
+			GetPublicKey(serverConnection);
+
+			String? messg;
+			do {
+				messg = ReadText("Enter message to be sent to server : ");
+
+				messg = (messg.Replace(":", "-")).ToUpper();
+				String encrypted = Encrypt(messg);
+				serverConnection.Send(Encoding.ASCII.GetBytes(encrypted));
+			} while (messg != "STOP");
+
+			//close gracefully:
+			try {
+				serverConnection.Shutdown(SocketShutdown.Both);
+			} finally {
+				serverConnection.Close();
+			}
+		}
+
+		static private String ReadText(String lead) {
+			//Pre : None
+			//Post: Returns a string read from keyboard with guaranteed content 
+			String? txt;
+			do {
+				Console.Write(lead);
+				txt = Console.ReadLine();
+				if (String.IsNullOrEmpty(txt)) {
+					Console.WriteLine("Please enter some text!");
+				}
+			} while (txt == null); //must have content
+			return txt;
+		}
+
+		private String Encrypt(String messg) {
+			//Pre : N and E holds values for encryption (public key from server), messg must have content
+			//Post: messg is encoded as bytes, each byte is encrypted and concatenated separated by ":"
+			String encrypted = "";
+			
+			byte[] asciiBytes = Encoding.ASCII.GetBytes(messg);
+			foreach (byte asciiByte in asciiBytes) {
+				BigInteger bi = new BigInteger(asciiByte);
+				encrypted += ((BigInteger.Pow(bi, E) % N).ToString() + ":");
+			}
+			encrypted = encrypted.Remove(encrypted.Length - 1); //remove last ":" 
+
+			return encrypted;
+		}
+
+		private void GetPublicKey(Socket serverConn) {
+			//Pre : serverConn holds an active connection
+			//Post: Next message (several parts separated by ":") from serverConn is read.
+			//      N and E is set to the first two elements from message
+			String[] parts = GetMessage(serverConn);
+			N = int.Parse(parts[0]);
+			E = int.Parse(parts[1]);
+		}
+
+		static private String[] GetMessage(Socket serverConn) {
+			//Pre : serverConn holds an active connection
+			//Post: A message is read from serverConn and changed into upper case. 
+			//      Message can be several parts separated by ":" so it is split into array
+			byte[] data = new byte[serverConn.SendBufferSize];
+
+			int receivedBytes = serverConn.Receive(data);
+			String receivedMessage = Encoding.ASCII.GetString(data, 0, receivedBytes).ToUpper();
+			return receivedMessage.Split(":");
+		}
+
+		static private Socket GetConnection() {
+			//Pre : None
+			//Post: If a server listens on ip:port, a socket connected to it is returned
+			String? ip;
+			String? port;
+			IPAddress address;
+			Socket result = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			bool done = false;
+			do {
+				ip = ReadText(" Enter IPv4 for server: ");
+				port = ReadText(" Enter port for server: ");
+
+				try {
+					address = IPAddress.Parse(ip);
+					IPEndPoint server = new IPEndPoint(address, Int32.Parse(port));
+					result.Connect(server);
+					done = true;
+				} catch (Exception) {
+					Console.WriteLine($"\nCould not connect to {ip}");
+				}
+			} while (!done);
+			return result;
+		}
+	}
+}
+
---
+++ Semester 3/Teknologi-faget/ClientCommunication/Server/Server.cs
@@ -0,0 +1,166 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+
+namespace Server {
+	internal class Server {
+		internal IPAddress? ip;
+		internal const int PORT = 1234;
+		internal readonly DecryptData DD = new DecryptData(4819, 41, 3881);
+		internal Socket? listener;
+		static bool running = true;
+
+		static void Main() {
+			Server p = new Server();
+			p.Run();
+		}
+
+		private void Run() {
+			//Pre : None
+			//Post: Set up a listening socket. 
+			//      Accepting clients in a loop, 
+			//      For each client: Create and start a thread that runs ServeClient(...)
+
+			Socket? client;			
+			listener = GetListener();
+
+			do {
+				Console.Write("Waiting for client ... ");
+				
+				
+			    //FILL IN CODE TO FULFILL POST-CONDITION HERE
+			
+			
+			} while (running);
+			//it is not obvious how running is set to false... Is to be fixed later!
+			
+			listener.Close();
+		}
+
+		private void ServeClient(Object? c) {
+			//Pre : c is a connected socket 
+			//Post: As "c" has type Object it must first be casted into a Socket 
+			//      variable called "client". 
+			//      First message from client are read using "GetMessage()", it is 
+			//      stored in a "String" variable called "id". 
+			//      Then public key ("DD.N" and "DD.E" separated by the char ':') is  
+			//      sent to client. 
+			//      Finally - in a loop - encrypted messages are read using "GetMessage()", 
+			//      written on console, decrypted using "Decrypt()" and written on console in
+			//		ecrypted form in upper case, until message equals "STOP".
+			//		Connection is closed and metod ends.
+			
+				
+			    //FILL IN CODE TO FULFILL POST-CONDITION HERE
+			
+			
+
+			String[] encryptedMessage;
+			String decryptedMessage;
+			do {
+				
+				
+				
+			    //FILL IN CODE TO FULFILL POST-CONDITION HERE
+			
+			
+			
+			} while (decryptedMessage != "STOP");
+
+			try {
+				client.Shutdown(SocketShutdown.Both);
+			} finally {
+				client.Close();
+			}
+		}
+
+		private String Decrypt(String[] parts) {
+			//Pre : DD holds values for decrypting (private key). 
+			//Post: All strings in parts are decrypted using DD and concatenated into one string
+			
+			BigInteger bi;
+			List<Char> chars = new List<Char>();
+
+			foreach (String s in parts) {
+				//decrypt
+				bi = BigInteger.Pow(Int32.Parse(s), DD.D) % DD.N;
+				char c = Convert.ToChar((int)bi);
+				chars.Add(c);
+			}
+			return new String(chars.ToArray());
+		}
+
+		static private String[] GetMessage(Socket client) {
+			//Pre : client holds an active connection
+			//Post: A message is read from client and changed into upper case. 
+			//      Message can be several parts separated by ":" so it is split into array
+			
+			byte[] data = new byte[client.SendBufferSize];
+			int receivedBytes = client.Receive(data);
+			String receivedMessage = (Encoding.ASCII.GetString(data, 0, receivedBytes).ToUpper());
+
+			return receivedMessage.Split(":");
+		}
+
+		private Socket GetListener() {
+			//Pre : None
+			//Post: A socket listening on a chosen IP-addres and port is returned
+			ip = GetIPAddress();
+			listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			Console.WriteLine($"Listening on : {ip} using port: {PORT}");
+			listener.Bind(new IPEndPoint(ip, PORT));
+			listener.Listen();
+
+			return listener;
+		}
+
+		static private IPAddress GetIPAddress() {
+			//Pre : None
+			//Post: From a menu showing all IPv4 addresses one is selected and returned
+			int indx = 1;
+
+			List<IPAddress> myIP4s = LoadIPv4();
+			foreach (IPAddress ip in myIP4s) {
+				ChColor(ConsoleColor.DarkYellow, ConsoleColor.Black);
+				Console.Write($" {indx}: ");
+				ChColor(ConsoleColor.Black, ConsoleColor.White);
+				Console.WriteLine($" {ip} ");
+				indx++;
+			}
+			Console.Write("\n\n Input number : ");
+
+			if (Int32.TryParse(Console.ReadKey().KeyChar.ToString(), out int choice)) {
+				Console.Clear();
+				return myIP4s[choice - 1];
+			} else {
+				throw new FormatException();
+			}
+		}
+
+		static private List<IPAddress> LoadIPv4() {
+			//Pre : None
+			//Post: All IPv4 (= AddressFamily.InterNetwork) addresses
+			//      on host is returned in a List
+			string hostName = Dns.GetHostName();
+			IPAddress[] myIPs = Dns.GetHostEntry(hostName).AddressList;
+
+			List<IPAddress> myIP4s = new List<IPAddress>();
+			foreach (IPAddress ip in myIPs) {
+				if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
+					myIP4s.Add(ip);
+				}
+			}
+			return myIP4s;
+		}
+
+		static private void ChColor(ConsoleColor bg, ConsoleColor fg) {
+			//Pre : None
+			//Post: Changed colors to bg and fg
+			Console.BackgroundColor = bg;
+			Console.ForegroundColor = fg;
+		}
+	}
+}
+
