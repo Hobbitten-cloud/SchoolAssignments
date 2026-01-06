9cc038f583eaf26150bcd56e43281cbf4abc35d0
Hobbitten <janenhansen@gmail.com>
2026-01-06 15:46:50 +0100

    Added 2 new exams projects

---
+++ Eksam/EksamMVC/Controllers/StudieController.cs
@@ -0,0 +1,19 @@
+﻿using EksamMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EksamMVC.Controllers
+{
+    public class StudieController : Controller
+    {
+        public IActionResult Index()
+        {
+            var study = new List<studiegruppemedlemmer>() 
+            { 
+                new studiegruppemedlemmer { Name = "hejsa", Age = 43},
+                new studiegruppemedlemmer { Name = "yes", Age = 454}
+            };
+
+            return View(study);
+        }
+    }
+}
---
+++ Eksam/EksamMVC/Models/studiegruppemedlemmer.cs
@@ -0,0 +1,13 @@
+﻿namespace EksamMVC.Models
+{
+    public class studiegruppemedlemmer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public studiegruppemedlemmer()
+        {
+
+        }
+    }
+}
